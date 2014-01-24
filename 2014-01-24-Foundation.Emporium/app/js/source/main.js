99a1fafd3321537cadc64b6baff2e798c820ccb1
Tyler Malone <rtylermalone@gmail.com>
2014-01-24 14:57:23 -0600

    Debugged Emporium code

--- 2014-01-24-Foundation.Emporium/app/js/source/main.js
+++ 2014-01-24-Foundation.Emporium/app/js/source/main.js
@@ -10,23 +10,49 @@
 
   function addItem(event){
     var item = $('#item').val();
-    var qty = $('#qty').val() * 1;
-    var amt = $('#amt').val() * 1;
+    var qty = $('#qty').val();
+    var amt = $('#amt').val();
     var total = qty * amt;
     addItemToTable(item, qty, amt, total);
+    updateTotals();
     event.preventDefault();
-    debugger;
+  }
+
+  function updateTotals(){
+    var $amts = $('table > tbody > tr > td:nth-child(3)');
+    var numbers = transformTdsToNums($amts);
+    var sumAmts = sum(numbers);
+    $('table > tfoot > tr > td:nth-child(3)').text(numberToCurrency(sumAmts));
+
+    var $totals = $('table > tbody > tr > td:nth-child(4)');
+    numbers = transformTdsToNums($totals);
+    sumAmts = sum(numbers);
+    $('table > tfoot > tr > td:nth-child(4)').text(numberToCurrency(sumAmts));
+  }
+
+  function sum(numbers){
+    var total = 0;
+    for(var i=0; i<numbers.length; i++){
+      total += numbers[i];
+    }
+    return total;
+  }
+
+  function transformTdsToNums($tds){
+    return $.map($tds, function(td){
+      return td.textContent.slice(1) * 1;
+    });
   }
 
   function addItemToTable(item, qty, amt, total){
     var $tr = $('<tr>');
-    var $item = ('<td>');
+    var $item = $('<td>');
     $item.text(item);
-    var $qty = ('<td>');
+    var $qty = $('<td>');
     $qty.text(qty);
-    var $amt = ('<td>');
+    var $amt = $('<td>');
     $amt.text(numberToCurrency(amt * 1));
-    var $total = ('<td>');
+    var $total = $('<td>');
     $total.text(numberToCurrency(total));
 
     $tr.append($item, $qty, $amt, $total);
